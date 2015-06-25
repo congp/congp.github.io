10ce281a6c7c7880ef3578d0c249035cdb42eedf
Cong Pham <cong@misfit.com>
2015-06-25 15:18:25 +0700

    Initial commit.

---
+++ _site/assets/js/congphamdesign.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    setupRotator();
+});
+
+function setupRotator() {
+    if ($('.rand-item').length > 1) {
+        $('.rand-item:first').addClass('current').fadeIn(500);
+        var interval = setInterval('textRotate()', 5000);
+    }
+}
+
+function textRotate() {
+    var current = $('.rand > .current');
+    if (current.next().length == 0) {
+        current.removeClass('current').fadeOut(500);
+        current.dequeue();
+        $('.rand-item:first').addClass('current').fadeIn(500);
+    } else {
+        current.removeClass('current').fadeOut(500);
+        current.dequeue();
+        current.next().addClass('current').fadeIn(500);
+    }
+}
